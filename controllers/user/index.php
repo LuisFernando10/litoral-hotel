6bbd25a4b768fa05994f2a20100f297a642c5304
Luis Chamorro <ing.lfchamorro@gmail.com>
2020-01-27 22:34:56 -0500

    Se faz configuraçao e implementaçao completamente funcional do Twig no projeto (Configuraçao)

--- controllers/user/index.php
+++ controllers/user/index.php
@@ -7,20 +7,37 @@
          * @Date: 26-ene-2020
          */
 
-//        //Incluimos la configuraciòn del gestor de paquetes del 'Composer'
-//        require_once (dirname(__FILE__).'/../../vendor/autoload.php');
+        //Incluimos la configuraciòn del gestor de paquetes del 'Composer'
+        require_once (dirname(__FILE__).'/../../vendor/autoload.php');
 //
-//        //Cargamos configuraciòn e indicamos en què parte se deben renderizar las vistas del 'Twig'
-//        $loader = new \Twig\Loader\FilesystemLoader(dirname(__FILE__).'/../../views/user/');
-//
-//        //Cargamos las configuraciones para el ambiente de las plantillas
-//        $twig = new \Twig\Environment($loader, [
-//            'cache' => dirname(__FILE__).'/../../views/user/'
-//        ]);
-//
-//        //Obtenemos los paràmetros URL configurados desde el .htaccess (Clase-Acciòn-Id)
-//        $class = filter_input(INPUT_GET, 'class', FILTER_SANITIZE_STRING, array("options" => array("default" => "home")));
-//        $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING, array("options" => array("default" => "")));
-//        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT, array("options" => array("default" => "")));
+        //Cargamos configuraciòn e indicamos en què parte se deben renderizar las vistas del 'Twig'
+        $loader = new \Twig\Loader\FilesystemLoader(dirname(__FILE__).'/../../views/user/');
 
-        echo $class;
+        //Cargamos las configuraciones para el ambiente de las plantillas
+        $twig = new \Twig\Environment($loader, [
+            //'cache' => dirname(__FILE__).'/../../views/user/',
+            'auto_reload' => true
+        ]);
+
+        //Obtenemos los paràmetros URL configurados desde el .htaccess (Clase-Acciòn-Id)
+        $class = filter_input(INPUT_GET, 'class', FILTER_SANITIZE_STRING, array("options" => array("default" => "home")));
+        $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING, array("options" => array("default" => "")));
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT, array("options" => array("default" => "")));
+
+        //Definimos las variables generales a utilizar en las vistas 'Twig'
+        $generalParam = array(
+            "full_web_url" => constant('FULL_WEB_URL'),
+            "full_assets_url" => constant('ASSETS_WEB_URL'),
+            "full_images_url" => constant('IMAGES_WEB_URL')
+        );
+
+        //Evaluamos cada uno de los controladores para permitir el acceso a las respectivas vistas
+        switch ($class){
+            case 'home':
+
+                //Renderizamos la vista
+                $twig->display('home.twig', array(
+                    'general' => $generalParam
+                ));
+                break;
+        }
