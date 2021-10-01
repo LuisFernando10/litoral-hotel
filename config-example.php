728865e3bba76b76949561b6f0e65ed877aa5cac
Luis Fernando <desarrollo@fcomunicaciones.com>
2021-10-01 10:45:36 -0500

    Se agrega archivo de configuración de ejemplo y se actualiza el git.ignore

--- config-example.php
+++ config-example.php
@@ -7,7 +7,7 @@
     define('SITE_PROTOCOL', isset($_SERVER['HTTPS']) === true ? 'https' : 'http');
     define("PAGINATION", 15);
     define('DEBUG', true);
-    define('EMAIL', 'reservas@hotellitoral.com.br');
+    define('EMAIL', '');
 
     #Rutas
     define('FULL_WEB_URL', constant('SITE_PROTOCOL').'://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['SCRIPT_NAME']).'/');
@@ -15,7 +15,7 @@
     define('IMAGES_WEB_URL', constant('ASSETS_WEB_URL').'img/');
 
     #BD
-    define('DB_SERVER','litora-hotel.mysql.uhserver.com'); //litora-hotel.mysql.uhserver.com - localhost
-    define('DB_USER','hlitorallira');//phpmyadmin - root - hlitorallira
-    define('DB_PASSWORD','HlLitoHote10f*');//lfchamorro10f - root - HlLitoHote10f*
-    define('DB_DATABASE','litora_hotel');//litoral_hotel - litora_hotel
+    define('DB_SERVER','');
+    define('DB_USER','');
+    define('DB_PASSWORD','');
+    define('DB_DATABASE','');
