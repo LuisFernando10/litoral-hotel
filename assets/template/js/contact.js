1d8991d131edccd55704f25e997cec8a1b56c398
Luis Fernando <dev.lfchamorro@gmail.com>
2024-03-03 21:19:26 -0500

    Changes for Phones and redirections were maked

--- assets/template/js/contact.js
+++ assets/template/js/contact.js
@@ -13,163 +13,144 @@
 
 ******************************/
 
-$(document).ready(function()
-{
-	"use strict";
+$(document).ready(function () {
+  "use strict";
 
-	/* 
+  /* 
 
 	1. Vars and Inits
 
 	*/
 
-	var header = $('.header');
-	var map;
+  var header = $(".header");
+  var map;
 
-	setHeader();
+  setHeader();
 
-	$(window).on('resize', function()
-	{
-		setHeader();
+  $(window).on("resize", function () {
+    setHeader();
 
-		setTimeout(function()
-		{
-			$(window).trigger('resize.px.parallax');
-		}, 375);
-	});
+    setTimeout(function () {
+      $(window).trigger("resize.px.parallax");
+    }, 375);
+  });
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+  $(document).on("scroll", function () {
+    setHeader();
+  });
 
-	initMenu();
-	initDatePicker();
-	initGoogleMap();
+  initMenu();
+  initDatePicker();
+  initGoogleMap();
 
-	/* 
+  /* 
 
 	2. Set Header
 
 	*/
 
-	function setHeader()
-	{
-		if($(window).scrollTop() > 91)
-		{
-			header.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-		}
-	}
+  function setHeader() {
+    if ($(window).scrollTop() > 91) {
+      header.addClass("scrolled");
+    } else {
+      header.removeClass("scrolled");
+    }
+  }
 
-	/* 
+  /* 
 
 	3. Init Menu
 
 	*/
 
-	function initMenu()
-	{
-		if($('.menu').length)
-		{
-			var menu = $('.menu');
-			var hamburger = $('.hamburger');
-			var close = $('.menu_close');
+  function initMenu() {
+    if ($(".menu").length) {
+      var menu = $(".menu");
+      var hamburger = $(".hamburger");
+      var close = $(".menu_close");
 
-			hamburger.on('click', function()
-			{
-				menu.toggleClass('active');
-			});
+      hamburger.on("click", function () {
+        menu.toggleClass("active");
+      });
 
-			close.on('click', function()
-			{
-				menu.toggleClass('active');
-			});
-		}
-	}
+      close.on("click", function () {
+        menu.toggleClass("active");
+      });
+    }
+  }
 
-	/* 
+  /* 
 
 	4. Init Date Picker
 
 	*/
 
-	function initDatePicker()
-	{
-		if($('.datepicker').length)
-		{
-			var datePickers = $('.datepicker');
-			datePickers.each(function()
-			{
-				var dp = $(this);
-				// Uncomment to use date as a placeholder
-				// var date = new Date();
-				// var dateM = date.getMonth() + 1;
-				// var dateD = date.getDate();
-				// var dateY = date.getFullYear();
-				// var dateFinal = dateM + '/' + dateD + '/' + dateY;
-				var placeholder = dp.data('placeholder');
-				dp.val(placeholder);
-				dp.datepicker();
-			});
-		}	
-	}
+  function initDatePicker() {
+    if ($(".datepicker").length) {
+      var datePickers = $(".datepicker");
+      datePickers.each(function () {
+        var dp = $(this);
+        // Uncomment to use date as a placeholder
+        // var date = new Date();
+        // var dateM = date.getMonth() + 1;
+        // var dateD = date.getDate();
+        // var dateY = date.getFullYear();
+        // var dateFinal = dateM + '/' + dateD + '/' + dateY;
+        var placeholder = dp.data("placeholder");
+        dp.val(placeholder);
+        dp.datepicker();
+      });
+    }
+  }
 
-	/* 
+  /* 
 
 	5. Init Google Map
 
 	*/
 
-	function initGoogleMap()
-	{
-		var myLatlng = new google.maps.LatLng(40.760836, -73.910357);
-    	var mapOptions = 
-    	{
-    		center: myLatlng,
-	       	zoom: 14,
-			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			draggable: true,
-			scrollwheel: false,
-			zoomControl: true,
-			zoomControlOptions:
-			{
-				position: google.maps.ControlPosition.RIGHT_CENTER
-			},
-			mapTypeControl: false,
-			scaleControl: false,
-			streetViewControl: false,
-			rotateControl: false,
-			fullscreenControl: true,
-			styles:
-			[
-			  {
-			    "featureType": "road.highway",
-			    "elementType": "geometry.fill",
-			    "stylers": [
-			      {
-			        "color": "#ffeba1"
-			      }
-			    ]
-			  }
-			]
-    	}
+  function initGoogleMap() {
+    var myLatlng = new google.maps.LatLng(
+      -10.982587710438722,
+      -37.04581083478648
+    );
+    var mapOptions = {
+      center: myLatlng,
+      zoom: 18,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      draggable: true,
+      scrollwheel: false,
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.RIGHT_CENTER,
+      },
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      rotateControl: false,
+      fullscreenControl: true,
+      styles: [
+        {
+          featureType: "road.highway",
+          elementType: "geometry.fill",
+          stylers: [
+            {
+              color: "#ffeba1",
+            },
+          ],
+        },
+      ],
+    };
 
-    	// Initialize a map with options
-    	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    // Initialize a map with options
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-		// Re-center map after window resize
-		google.maps.event.addDomListener(window, 'resize', function()
-		{
-			setTimeout(function()
-			{
-				google.maps.event.trigger(map, "resize");
-				map.setCenter(myLatlng);
-			}, 1400);
-		});
-	}
-
+    // Re-center map after window resize
+    google.maps.event.addDomListener(window, "resize", function () {
+      setTimeout(function () {
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(myLatlng);
+      }, 1400);
+    });
+  }
 });
